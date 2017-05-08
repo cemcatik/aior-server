04c1c96d3d3d9ff3fc7b1f15379981d9829abca9
Cem Catikkas <cem.catikkas@gmail.com>
2017-05-08 02:33:58 -0700

    Check the received commands are from previously handshaken client.

--- src/main/scala/com/catikkas/aiorserver/Server.scala
+++ src/main/scala/com/catikkas/aiorserver/Server.scala
@@ -12,13 +12,14 @@ class Server extends Actor with ActorLogging with Config {
   var robot = context.system.deadLetters
 
   override def preStart(): Unit = {
+    import context.system
     IO(Udp) ! Bind(self, new InetSocketAddress(config.port))
     robot = context.actorOf(Robot.props, "robot")
   }
 
   def receive = notBound
 
-  def notBound: Receive = LoggingReceive.withLabel("notBound") {
+  def notBound = LoggingReceive.withLabel("notBound") {
     case CommandFailed(_) =>
       log.error("failed to bind")
       context stop self
@@ -33,21 +34,24 @@ class Server extends Actor with ActorLogging with Config {
   import Messages.AiocId._
   import Robot._
 
-  def bound(socket: ActorRef): Receive = LoggingReceive.withLabel("bound") {
+  def bound(socket: ActorRef) = LoggingReceive.withLabel("bound") {
     case Received(Aioc(ConnectionReceived), remote) =>
       log.info("connection attempt from {}", remote)
       socket ! Send(UdpConnectionAccepted.toJson, new InetSocketAddress(remote.getAddress, config.port))
+      context become connected(remote)
+  }
 
-    case Received(MouseMove(x, y), _)         => robot ! MouseMoveDelta(x, y)
-    case Received(Aioc(MouseLeftPress), _)    => robot ! MousePress(MouseButton.Left)
-    case Received(Aioc(MouseLeftRelease), _)  => robot ! MouseRelease(MouseButton.Left)
-    case Received(Aioc(MouseRightPress), _)   => robot ! MousePress(MouseButton.Right)
-    case Received(Aioc(MouseRightRelease), _) => robot ! MouseRelease(MouseButton.Right)
-    case Received(Aioc(MouseWheelDown), _)    => robot ! MouseWheel(WheelDirection.Down)
-    case Received(Aioc(MouseWheelUp), _)      => robot ! MouseWheel(WheelDirection.Up)
-    case Received(KeyboardString(chars), _)   => robot ! PressKeys(chars)
-    case Received(KeyboardInt(int), _)        => robot ! PressKey(int)
-    case Received(m, remote)                  => log.debug("received unhandled {} from {}", m.utf8String, remote)
+  def connected(remote: InetSocketAddress) = LoggingReceive.withLabel(s"connected $remote") {
+    case Received(MouseMove(x, y), `remote`)         => robot ! MouseMoveDelta(x, y)
+    case Received(Aioc(MouseLeftPress), `remote`)    => robot ! MousePress(MouseButton.Left)
+    case Received(Aioc(MouseLeftRelease), `remote`)  => robot ! MouseRelease(MouseButton.Left)
+    case Received(Aioc(MouseRightPress), `remote`)   => robot ! MousePress(MouseButton.Right)
+    case Received(Aioc(MouseRightRelease), `remote`) => robot ! MouseRelease(MouseButton.Right)
+    case Received(Aioc(MouseWheelDown), `remote`)    => robot ! MouseWheel(WheelDirection.Down)
+    case Received(Aioc(MouseWheelUp), `remote`)      => robot ! MouseWheel(WheelDirection.Up)
+    case Received(KeyboardString(chars), `remote`)   => robot ! PressKeys(chars)
+    case Received(KeyboardInt(int), `remote`)        => robot ! PressKey(int)
+    case Received(m, r)                              => log.debug("received unhandled {} from {}", m.utf8String, r)
   }
 }
 
