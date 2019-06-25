d7757eb94e0104ecf2e8d99e414418aea2b81607
Cem Catikkas <cem.catikkas@gmail.com>
2019-06-25 09:08:55 -0700

    Refactored keyboard mapping to companion object.

--- src/main/scala/com/catikkas/aiorserver/Robot.scala
+++ src/main/scala/com/catikkas/aiorserver/Robot.scala
@@ -7,6 +7,7 @@ import java.awt.event.KeyEvent
 import akka.actor.typed._
 import akka.actor.typed.scaladsl._
 import com.catikkas.aiorserver.Main._
+import com.catikkas.aiorserver.Keyboard._
 import javax.swing.KeyStroke
 
 import scala.util._
@@ -126,61 +127,7 @@ object Robot {
   }
 
   def pressKey(int: Int)(implicit state: State, config: Config, robot: AwtRobot): Behavior[Command] = {
-    val int2keycode = Map(
-      29 -> KeyEvent.VK_A,
-      30 -> KeyEvent.VK_B,
-      31 -> KeyEvent.VK_C,
-      32 -> KeyEvent.VK_D,
-      33 -> KeyEvent.VK_E,
-      34 -> KeyEvent.VK_F,
-      35 -> KeyEvent.VK_G,
-      36 -> KeyEvent.VK_H,
-      37 -> KeyEvent.VK_I,
-      38 -> KeyEvent.VK_J,
-      39 -> KeyEvent.VK_K,
-      40 -> KeyEvent.VK_L,
-      41 -> KeyEvent.VK_M,
-      42 -> KeyEvent.VK_N,
-      43 -> KeyEvent.VK_O,
-      44 -> KeyEvent.VK_P,
-      45 -> KeyEvent.VK_Q,
-      46 -> KeyEvent.VK_R,
-      47 -> KeyEvent.VK_S,
-      48 -> KeyEvent.VK_T,
-      49 -> KeyEvent.VK_U,
-      50 -> KeyEvent.VK_V,
-      51 -> KeyEvent.VK_W,
-      52 -> KeyEvent.VK_X,
-      53 -> KeyEvent.VK_Y,
-      54 -> KeyEvent.VK_Z,
-      7  -> KeyEvent.VK_0,
-      8  -> KeyEvent.VK_1,
-      9  -> KeyEvent.VK_2,
-      10 -> KeyEvent.VK_3,
-      11 -> KeyEvent.VK_4,
-      12 -> KeyEvent.VK_5,
-      13 -> KeyEvent.VK_6,
-      14 -> KeyEvent.VK_7,
-      15 -> KeyEvent.VK_8,
-      16 -> KeyEvent.VK_9,
-      56 -> KeyEvent.VK_PERIOD,
-      70 -> KeyEvent.VK_EQUALS,
-      73 -> KeyEvent.VK_BACK_SLASH,
-      55 -> KeyEvent.VK_COMMA,
-      76 -> KeyEvent.VK_SLASH,
-      72 -> KeyEvent.VK_CLOSE_BRACKET,
-      71 -> KeyEvent.VK_OPEN_BRACKET,
-      62 -> KeyEvent.VK_SPACE,
-      66 -> KeyEvent.VK_ENTER,
-      59 -> KeyEvent.VK_SHIFT,
-      67 -> KeyEvent.VK_BACK_SPACE,
-      69 -> KeyEvent.VK_MINUS,
-      81 -> KeyEvent.VK_ADD,
-      74 -> KeyEvent.VK_SEMICOLON,
-      68 -> KeyEvent.VK_BACK_QUOTE
-    )
-
-    int2keycode(int) match {
+    Int2Keycode(int) match {
       case KeyEvent.VK_SHIFT =>
         initialized(state.copy(shiftPressed = true), config, robot)
       case x =>
@@ -215,4 +162,58 @@ object Keyboard {
   val KeyStrokes = Numbers ++ LowerAZ ++ UpperAZ ++ Whitespaces ++ Layout
 
   def keyStroke(c: Char): KeyStroke = KeyStrokes(c)
+
+  val Int2Keycode = Map(
+    29 -> KeyEvent.VK_A,
+    30 -> KeyEvent.VK_B,
+    31 -> KeyEvent.VK_C,
+    32 -> KeyEvent.VK_D,
+    33 -> KeyEvent.VK_E,
+    34 -> KeyEvent.VK_F,
+    35 -> KeyEvent.VK_G,
+    36 -> KeyEvent.VK_H,
+    37 -> KeyEvent.VK_I,
+    38 -> KeyEvent.VK_J,
+    39 -> KeyEvent.VK_K,
+    40 -> KeyEvent.VK_L,
+    41 -> KeyEvent.VK_M,
+    42 -> KeyEvent.VK_N,
+    43 -> KeyEvent.VK_O,
+    44 -> KeyEvent.VK_P,
+    45 -> KeyEvent.VK_Q,
+    46 -> KeyEvent.VK_R,
+    47 -> KeyEvent.VK_S,
+    48 -> KeyEvent.VK_T,
+    49 -> KeyEvent.VK_U,
+    50 -> KeyEvent.VK_V,
+    51 -> KeyEvent.VK_W,
+    52 -> KeyEvent.VK_X,
+    53 -> KeyEvent.VK_Y,
+    54 -> KeyEvent.VK_Z,
+    7  -> KeyEvent.VK_0,
+    8  -> KeyEvent.VK_1,
+    9  -> KeyEvent.VK_2,
+    10 -> KeyEvent.VK_3,
+    11 -> KeyEvent.VK_4,
+    12 -> KeyEvent.VK_5,
+    13 -> KeyEvent.VK_6,
+    14 -> KeyEvent.VK_7,
+    15 -> KeyEvent.VK_8,
+    16 -> KeyEvent.VK_9,
+    56 -> KeyEvent.VK_PERIOD,
+    70 -> KeyEvent.VK_EQUALS,
+    73 -> KeyEvent.VK_BACK_SLASH,
+    55 -> KeyEvent.VK_COMMA,
+    76 -> KeyEvent.VK_SLASH,
+    72 -> KeyEvent.VK_CLOSE_BRACKET,
+    71 -> KeyEvent.VK_OPEN_BRACKET,
+    62 -> KeyEvent.VK_SPACE,
+    66 -> KeyEvent.VK_ENTER,
+    59 -> KeyEvent.VK_SHIFT,
+    67 -> KeyEvent.VK_BACK_SPACE,
+    69 -> KeyEvent.VK_MINUS,
+    81 -> KeyEvent.VK_ADD,
+    74 -> KeyEvent.VK_SEMICOLON,
+    68 -> KeyEvent.VK_BACK_QUOTE
+  )
 }
